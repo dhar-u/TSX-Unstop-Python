a2ac681e69aaac0ce292471c1fca3acfc9254346
dhar-u <152897184+dharanika-u@users.noreply.github.com>
2025-05-18 13:25:00 +0530

    Added greeting scripts and commented example

---
+++ commented_script.py
@@ -0,0 +1,7 @@
+# This script shows how to store and print a name
+
+# Defining a variable to hold the name
+name = "Dharanika"
+
+# Printing a greeting message using the variable
+print("Hello, " + name + "! Welcome to Python.")
